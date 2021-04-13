097b61bd0f6f10f493f1b66fb6b62e52336b7184
Annie Li <liqy112008@gmail.com>
2021-04-13 12:49:04 +0800

    paginator

--- src/components/content/FetchPoke.js
+++ src/components/content/FetchPoke.js
@@ -1,9 +1,29 @@
 import React, { Component, useState, useEffect } from 'react'
 import PokeCard from './PokeCard'
+import Pagination from '@material-ui/lab/Pagination';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    paginator: {
+        justifyContent: "center",
+        padding: "20px"
+      }
+}));
 
 const FetchPoke = () => {
 
     const [statuses, setStatuses] = useState([]);
+
+    const classes = useStyles();
+    const itemsPerPage = 15;
+    const [page, setPage] = useState(1);
+    const [noOfPages] = useState(
+        Math.ceil(100 / itemsPerPage)
+    );
+
+    function handleChange(event, value) {
+        setPage(value);
+    };
 
     useEffect(() => {
         fetchKantoPokemon();
@@ -18,10 +38,10 @@ const FetchPoke = () => {
             .then(response => response.json())
             .then((pokeData) => {
                 console.log(pokeData)
-                setStatuses(statuses => [...statuses,pokeData])
+                setStatuses(statuses => [...statuses, pokeData])
                 console.log(statuses)
-                
-                
+
+
             })
     }
 
@@ -38,11 +58,29 @@ const FetchPoke = () => {
 
     return (
         <div>
-            {statuses && statuses.map(status => (
-         
-               <PokeCard data = {status} key = {status.id}/>
+
+            { statuses && statuses
+            .slice((page - 1) * itemsPerPage, page * itemsPerPage)
+            .map(status => (
+
+                <PokeCard data={status} key={status.id} />
 
             ))}
+
+            <Pagination
+                count={noOfPages}
+                page={page}
+                onChange={handleChange}
+                defaultPage={1}
+                color="secondary"
+                size="large"
+                showFirstButton
+                showLastButton
+                classes={{ ul: classes.paginator }}
+            />
+
+
+
 
         </div>
     )
