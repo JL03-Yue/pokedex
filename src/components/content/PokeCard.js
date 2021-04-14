b9fa90085353eb55b4e0ff926e4cbd575625ca0b
Annie Li <liqy112008@gmail.com>
2021-04-14 13:47:29 +0800

    add pokeman detail modal

--- src/components/content/PokeCard.js
+++ src/components/content/PokeCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState}from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -7,8 +7,12 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import PokeDetailCard from '../pokeDetail/PokeDetail'
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme)=>({
   root: {
     maxWidth: 245,
     margin:10,
@@ -19,12 +23,34 @@ const useStyles = makeStyles({
     height: 200,
     width: 200,
   },
-});
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 export default function MediaCard({data}) {
   const classes = useStyles();
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
+
+    <div>
     <Card className={classes.root}>
       <CardActionArea>
         <CardMedia
@@ -44,8 +70,37 @@ export default function MediaCard({data}) {
         <Button size="small" color="primary">
           Like
         </Button>
+
+        <Button size="small" color="primary" onClick={handleOpen}>
+          Detail
+        </Button>
         
       </CardActions>
     </Card>
+    
+
+    {/* pop up detail */}
+    <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <PokeDetailCard data ={data}/>
+          </div>
+        </Fade>
+      </Modal>
+    
+    </div>
+
+    
   );
 }
---
+++ src/components/pokeDetail/PokeDetail.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+    root: {
+        
+        padding:10,
+
+    },
+    media: {
+        height: 150,
+        width: 150,
+        justifyContent:'center',
+        
+    },
+});
+
+export default function PokeDetailCard({ data }) {
+    const classes = useStyles();
+
+    return (
+        <Card className={classes.root}>
+
+
+            <CardActionArea>
+                <CardMedia
+                    className={classes.media}
+                    image={"https://pokeres.bastionbot.org/images/pokemon/" + String(data.id) + ".png"}
+
+                />
+                <CardContent>
+
+                    {/* name */}
+                    <Typography gutterBottom variant="h5" component="h5">
+                        {data.forms[0].name}
+                    </Typography>
+
+
+                    {/* detail */}
+                    <Typography gutterBottom variant="body2" component="p">
+                        <p>Ability: {data.abilities[0].ability.name}</p>
+                    </Typography>
+
+                    <Typography gutterBottom variant="body2" component="p">
+                        <p>Move: {data.moves[0].move.name}</p>
+                    </Typography>
+
+                    <Typography gutterBottom variant="body2" component="p">
+                        <p>Weight: {data.weight}</p>
+                    </Typography>
+
+
+
+                </CardContent>
+            </CardActionArea>
+
+        </Card>
+    );
+}
